8b02798df7663d918e6800636461b7654f2ed6ba
tuannho <lntuan99@gmail.com>
2021-10-04 01:01:50 +0700

    ver2

--- AdapterDesign/Program.cs
+++ AdapterDesign/Program.cs
@@ -2,98 +2,85 @@
 
 namespace AdapterDesign
 {
-    // Đầu tiên, bạn có một interface bear như thế này 
-    public interface IBear
+    // Đầu tiên, bạn có một interface IMonitor như sau
+    public interface IMonitor
     {
-        void Skin();
-        void Roar();
+        public void MonitorConnector();
     }
 
-   
-    // Tạo một lớp gấu trúc implement interface IBear ở trên
-    public class Panda : IBear
+    // Tạo một lớp SamSungMonitor implement interface IMonitor ở trên với cách kết nối thông qua cổng HDMI
+    public class SamSungMonitor : IMonitor
     {
-        public void Skin()
+        public void HDMI()
         {
-            Console.WriteLine("Skin: My skin color is black and white");
+            Console.WriteLine("[Monitor]: HDMI cable connecting...");
         }
 
-        public void Roar()
+        public void MonitorConnector()
         {
-            Console.WriteLine("Roar: I'm kung fu pandaaaaa...");
-        }
-    }
-
-    // Và bạn cũng có một interface chó như sau
-    // Không giống như những chú gấu, những chú chó sẽ sủa chứ không gầm (Bark() thay vì Roar())
-    public interface IDog
-    {
-        void Skin();
-        void Bark();
-    }
-
-    // Một lớp chó shiba implement interface IDog
-    public class Shiba : IDog
-    {
-        public void Skin()
-        {
-            Console.Write("Skin: My skin color can be a lot of colors. ");
-        }
-
-        public void Bark()
-        {
-            Console.WriteLine("Bark: Go go go go go...");
+            HDMI();
         }
     }
 
-    // Và dây là khúc gay cấn nhất. biến một chú chó thành một chú gấu trúc nào
-    public class ShibaAdapter : IBear
+    // Và bạn cũng có một interface IVideoGraphicsArray như sau
+    public interface IVideoGraphicsArray
     {
-        Shiba shiba;
-        public ShibaAdapter(Shiba s)
+        public void VideoGraphicsArrayConnector();
+    }
+
+    // Một lớp RTX3060 implement interface IVideoGraphicsArray với cách kết nối thông qua cổng VGA
+    public class RTX3060 : IVideoGraphicsArray
+    {
+        public void VGA()
         {
-            shiba = s;
+            Console.WriteLine("[Card]: VGA cable connecting...");
         }
 
-        //Để cho giống một chú gấu trúc thì dễ nhìn ra nhất có lẻ là thay đổi màu lông của chúng
-        public void Skin()
+        public void VideoGraphicsArrayConnector()
         {
-            shiba.Skin();
-            Console.WriteLine("But my owner make my skin color become black and white");
+            VGA();
+        }
+    }
+
+    // Vấn đề đặt ra ở đây
+    // Người dùng muốn kết nối màn hình với card mà họ có
+    // Nhưng cổng kết nối của màn hình và card sử dụng là 2 cổng không giống nhau
+    // Và để kết nối 2 thiết bị thì người dùng phải tìm cách để biến cả 2 cổng kết nối của 2 thiết bị là giống nhau
+
+    // Ta cài đặt MonitorAdapter để chuyển cổng kết nối trên màn hìn thành cùng cổng kết nối như trên card mà ta sử dụng
+    public class MonitorAdapter : IVideoGraphicsArray
+    {
+        IMonitor monitor;
+        public MonitorAdapter(IMonitor m)
+        {
+            monitor = m;
         }
 
-        // Sẽ ra sao khi bạn bảo chú chó của bạn gầm lên ???
-        // Vâng. chúng không thể gầm, nhưng chúng có thể sủa
-        // Vì vậy, ở đây ta cho chú chó shiba của mình thực hiện phương thức Bark()
-        public void Roar()
+        public void VideoGraphicsArrayConnector()
         {
-            Console.Write("I'm roaring: ");
-            shiba.Bark();
+            Console.WriteLine("Monitor connector ====> Video Graphics Array connector");
+            monitor.MonitorConnector();
         }
     }
 
     class Program
     {
-        // Và ta có một hàm main như sau
+        // Ví dụ: Người dùng có một màn hình samsung với cổng kết nối là HDMI, 1 card RTX 3060 với cổng kết nối là VGA
+        // Nhung họ KHÔNG có 1 cable với 1 cổng HDMI, 1 cổng VGA
+        // Mà họ chỉ có 1 cable với cả 2 cổng là đều VGA
+        // Vì thế, họ đã sử dụng một đầu chuyển để chuyển đổi cổng HDMI trên màn hình thành cổng VGA 
+        // Và người dùng đã có thể kết nối 2 thiết bị với nhau bằng sợi cable có 2 cổng đều là VGA
         static void Main(string[] args)
         {
-           
-            // Và dây là những gì mà khách hàng của bạn muốn thấy
-            Panda realPanda = new Panda();
-            Console.WriteLine(">>           This is a real panda             <<");
-            realPanda.Skin();
-            realPanda.Roar();
-            Console.WriteLine("---------------------------\n");
+            SamSungMonitor monitor = new SamSungMonitor();
+            RTX3060 card = new RTX3060();
 
-            // Nhưng bạn chỉ có một chú chó shiba
-            // Và thông qua adapter, bạn đã biến chú shiba của mình thành một chú gấu trúc
+            // Gắn adapter, chuyển đổi cổng HDMI trên màn hình thành cổng VGA
+            MonitorAdapter adapter = new MonitorAdapter(monitor);
 
-            Shiba shiba = new Shiba();
-            ShibaAdapter shibaFakePanda = new ShibaAdapter(shiba);
-            Console.WriteLine(">>           This is a fake panda             <<");
-            shibaFakePanda.Skin();
-            shibaFakePanda.Roar();
-            Console.WriteLine("---------------------------");
+            // Sau khi đã chuyển đổi cổng trên màn hình thành cổng cùng một loại cổng với card thì ta đã có thể kết nổi thành công
+            card.VideoGraphicsArrayConnector();
+            adapter.VideoGraphicsArrayConnector();
         }
     }
 }
