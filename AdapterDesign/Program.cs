f4bb9167170e7413dd6aafd2267f1b1a792ad501
tuannho <lntuan99@gmail.com>
2021-10-04 22:18:33 +0700

    commit

--- AdapterDesign/Program.cs
+++ AdapterDesign/Program.cs
@@ -2,85 +2,97 @@
 
 namespace AdapterDesign
 {
-    // Đầu tiên, bạn có một interface IMonitor như sau
-    public interface IMonitor
+    public interface IProjector
     {
-        public void MonitorConnector();
+        public void connectHDMIPort();
+        public IProjector resizeImage();
     }
 
-    // Tạo một lớp SamSungMonitor implement interface IMonitor ở trên với cách kết nối thông qua cổng HDMI
-    public class SamSungMonitor : IMonitor
+    public class LCDProjector : IProjector
     {
-        public void HDMI()
+        public void connectHDMIPort()
         {
-            Console.WriteLine("[Monitor]: HDMI cable connecting...");
+            Console.WriteLine("connect LCD");
+            Console.WriteLine("[LCD] HDMI connecting...\n");
         }
 
-        public void MonitorConnector()
+        public IProjector resizeImage()
         {
-            HDMI();
-        }
-    }
-
-    // Và bạn cũng có một interface IVideoGraphicsArray như sau
-    public interface IVideoGraphicsArray
-    {
-        public void VideoGraphicsArrayConnector();
-    }
-
-    // Một lớp RTX3060 implement interface IVideoGraphicsArray với cách kết nối thông qua cổng VGA
-    public class RTX3060 : IVideoGraphicsArray
-    {
-        public void VGA()
-        {
-            Console.WriteLine("[Card]: VGA cable connecting...");
-        }
-
-        public void VideoGraphicsArrayConnector()
-        {
-            VGA();
+            Console.WriteLine("[LCD] resize image...\n");
+            return this;
         }
     }
 
-    // Vấn đề đặt ra ở đây
-    // Người dùng muốn kết nối màn hình với card mà họ có
-    // Nhưng cổng kết nối của màn hình và card sử dụng là 2 cổng không giống nhau
-    // Và để kết nối 2 thiết bị thì người dùng phải tìm cách để biến cả 2 cổng kết nối của 2 thiết bị là giống nhau
-
-    // Ta cài đặt MonitorAdapter để chuyển cổng kết nối trên màn hìn thành cùng cổng kết nối như trên card mà ta sử dụng
-    public class MonitorAdapter : IVideoGraphicsArray
+    public class LEDProjector : IProjector
     {
-        IMonitor monitor;
-        public MonitorAdapter(IMonitor m)
+        public void connectHDMIPort()
         {
-            monitor = m;
+            Console.WriteLine("connect LED");
+            Console.WriteLine("[LED] HDMI connecting...\n");
         }
 
-        public void VideoGraphicsArrayConnector()
+        public IProjector resizeImage()
         {
-            Console.WriteLine("Monitor connector ====> Video Graphics Array connector");
-            monitor.MonitorConnector();
+            Console.WriteLine("[LED] resize image...\n");
+
+            return this;
         }
     }
+
+    public class ScreenProjector : IProjector
+    {
+        TVScreen tvScreen;
+        public ScreenProjector(TVScreen tv)
+        {
+            tvScreen = tv;
+        }
+
+        public void connectHDMIPort()
+        {
+            tvScreen.connectVGAPort();
+        }
+
+        public IProjector resizeImage()
+        {
+            Console.WriteLine("[TV] resize image...\n");
+
+            return new ScreenProjector(tvScreen);
+        }
+    }
+
+
+    public class TVScreen 
+    {
+        public void connectVGAPort()
+        {
+            Console.WriteLine("connect TVScreen");
+            Console.WriteLine("[TV] VGA connecting...\n");
+        }
+    }
+
 
     class Program
     {
-        // Ví dụ: Người dùng có một màn hình samsung với cổng kết nối là HDMI, 1 card RTX 3060 với cổng kết nối là VGA
-        // Nhung họ KHÔNG có 1 cable với 1 cổng HDMI, 1 cổng VGA
-        // Mà họ chỉ có 1 cable với cả 2 cổng là đều VGA
-        // Vì thế, họ đã sử dụng một đầu chuyển để chuyển đổi cổng HDMI trên màn hình thành cổng VGA 
-        // Và người dùng đã có thể kết nối 2 thiết bị với nhau bằng sợi cable có 2 cổng đều là VGA
+
         static void Main(string[] args)
         {
-            SamSungMonitor monitor = new SamSungMonitor();
-            RTX3060 card = new RTX3060();
+            TVScreen tv = new TVScreen();
+            LEDProjector led = new LEDProjector();
+            LCDProjector lcd = new LCDProjector();
 
-            // Gắn adapter, chuyển đổi cổng HDMI trên màn hình thành cổng VGA
-            MonitorAdapter adapter = new MonitorAdapter(monitor);
+            ScreenProjector screenProjector = new ScreenProjector(tv);
 
-            // Sau khi đã chuyển đổi cổng trên màn hình thành cổng cùng một loại cổng với card thì ta đã có thể kết nổi thành công
-            card.VideoGraphicsArrayConnector();
-            adapter.VideoGraphicsArrayConnector();
+            Console.WriteLine(">>-----------CONNECT-----------<<");
+            led.connectHDMIPort();
+            lcd.connectHDMIPort();
+            screenProjector.connectHDMIPort();
+            Console.WriteLine(">>-----------------------------<<\n");
+
+            Console.WriteLine(">>---------RESIZE IMAGE---------<<");
+            led.connectHDMIPort();
+            lcd.connectHDMIPort();
+            screenProjector.connectHDMIPort();
+            Console.WriteLine(">>------------------------------<<");
         }
     }
 }
--- Readme.md
+++
@@ -1,8 +0,0 @@
-﻿Hãy tưởng tượng, bạn mở 1 cửa tiệm kinh daonh cà phê.
-Mô hình cà phê chó mèo đã quá nổi tiếng.
-Và điều làm cho quán của bạn đặc biệt đó chính là khách tham quan có thể vừa uống cà phê vùa nhìn GẤU TRÚC.
-Vâng, đó chính xác là GẤU TRÚC. Nhưng để sở hữu cho mình được một chú gấu trúc thì tôi đảm bảo rằng chính quyền nhà nước sẽ để ý đến bạn rất nhiều đấy.
-Vậy thì làm cách nào để bạn có thể thu hút được khách hàng đúng như ý nghĩ của mình??
-Sẽ ra sao nếu bạn biến một chú chó shiba trở thành một chú gấu trúc =))) 
-Thông qua lập trình và mẫu adapter, bạn hoàn toàn có thể làm được điều đó.
-Cùng xem nhé
